1d00cd142ea8fe5ed25d4ecc5387a45743c68db7
Mad-User <kotov.andrey01@mail.ru>
2020-03-30 01:19:03 +0300

    refactored tests

--- src/test/java/icc/stud/kotov_av/grep/GrepTest.java
+++ src/test/java/icc/stud/kotov_av/grep/GrepTest.java
@@ -1,16 +1,27 @@
 package icc.stud.kotov_av.grep;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
 
 public class GrepTest {
 
   @Test
   public void mainTest() {
-    String[] args = {"-i", "-V", "2", "3"};
-    Grep.main(args);
+    String[] args = {"-i", "-V", "hdgc", "file.txt"};
+    DataHolder holder = Grep.parseArguments(args);
+    assertTrue(holder.caseIgnore);
+    assertTrue(holder.inversion);
+    assertEquals(holder.word, "hdgc");
+    assertEquals(holder.inputFile, "file.txt");
 
-    args = new String[] {"yhvlyv", "obluyv"};
-    Grep.main(args);
+    // args = new String[] {"yhvlyv", "obluyv"};
+    // Grep.main(args);
+
+    // args = new String[] {"yhvlyv", "obluyv", "sdgsdg"};
+    // Grep.main(args);
   }
 
 }
