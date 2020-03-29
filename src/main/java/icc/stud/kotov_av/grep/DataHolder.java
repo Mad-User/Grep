fa6180b5435980bca3a0bc9d32126d02e063dc36
Mad-User <kotov.andrey01@mail.ru>
2020-03-30 01:20:05 +0300

    delited args4j code

--- src/main/java/icc/stud/kotov_av/grep/DataHolder.java
+++ src/main/java/icc/stud/kotov_av/grep/DataHolder.java
@@ -1,15 +1,11 @@
 package icc.stud.kotov_av.grep;
 
-import org.kohsuke.args4j.Option;
-
 public class DataHolder {
-    @Option(name="-r", aliases="-R", usage="...")
+
     public boolean regex;
  
-    @Option(name="-v", aliases="-V", usage="...")  
     public boolean inversion;
  
-    @Option(name="-i", aliases="-I", usage="...")
     public boolean caseIgnore;
 
     public String word;
