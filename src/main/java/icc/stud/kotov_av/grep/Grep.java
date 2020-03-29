62aa90ce42beb2efdfc0e1b36008642bf761aeb1
Mad-User <kotov.andrey01@mail.ru>
2020-03-29 22:58:26 +0300

    add parseArguments

--- src/main/java/icc/stud/kotov_av/grep/Grep.java
+++ src/main/java/icc/stud/kotov_av/grep/Grep.java
@@ -1,11 +1,34 @@
 package icc.stud.kotov_av.grep;
 
-import org.kohsuke.args4j.*;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 
-public class Grep {
+public class Grep {  
 
-  public static void main(String[] args) {
+  private static DataHolder parseArguments(final String[] args) {
+    final DataHolder dataHolder = new DataHolder();
+    final CmdLineParser parser = new CmdLineParser(dataHolder);
 
+    if (args.length < 2 || args.length > 5) {
+      System.out.println("Bad arguments...");
+      System.exit(-1);
+    } 
+    else {
+      try {
+        parser.parseArgument(args);
+      } catch (final CmdLineException clEx) {
+        System.out.println("Bad arguments...");
+        System.exit(-1);
+      }
+
+      dataHolder.word = args[args.length - 2];
+      dataHolder.inputFile = args[args.length - 1];
+    }
+
+    return dataHolder;
   }
 
+  public static void main(final String[] args) {
+    parseArguments(args);
+  }
 }
