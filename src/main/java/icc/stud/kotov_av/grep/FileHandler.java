e5a1f7ff4e5b2c486a616b917e64538f07b9f07b
Mad-User <kotov.andrey01@mail.ru>
2020-04-01 20:00:53 +0300

    algorithm is ready

--- src/main/java/icc/stud/kotov_av/grep/FileHandler.java
+++ src/main/java/icc/stud/kotov_av/grep/FileHandler.java
@@ -1,38 +1,46 @@
 package icc.stud.kotov_av.grep;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class FileHandler {
-    ArrayList<String> lines;
+	ArrayList<String> lines;
 
-    public FileHandler(ArgumentsHolder ah) {
+	public FileHandler() {
+		lines = new ArrayList<String>();
+	}
 
-    }
+	public void print() {
+		for (String line : lines) {
+			System.out.println(line);
+		}
+	}
 
-    public void print() {
-        for (String line : lines) {
-            System.out.print(line);
-        }
-    }
+	public String[] getLines() {
+		return (String[]) lines.toArray();
+	}
 
-    public ArrayList<String> getLines() {
-        return lines;
-    }
+	public void execute(ArgumentsHolder ah, BufferedReader br) throws Exception {
+		int flags = ah.caseIgnore ? Pattern.CASE_INSENSITIVE : 0;
+		Pattern pattern = ah.regex ? Pattern.compile(ah.word, flags) : null;
 
-    private void execute(ArgumentsHolder ah) {
-        try(BufferedReader br = new BufferedReader(new FileReader(ah.inputFile)))
-        {
-            //чтение построчно
-            String line;
-            while((line=br.readLine())!=null){
-                lines.add(line);
-            }
-        }
-        catch(IOException ex){       
-            System.out.println(ex.getMessage());
-        }
-    }
+		String search = ah.caseIgnore ? ah.word.toLowerCase() : ah.word;
+
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			if (pattern != null) {
+				if (pattern.matcher(line).matches() ^ ah.inversion)
+					lines.add(line);
+			} else {
+				if (ah.caseIgnore) {
+					if (line.toLowerCase().contains(search) ^ ah.inversion)
+						lines.add(line);
+				} else {
+					if (line.contains(search) ^ ah.inversion)
+						lines.add(line);
+				}
+			}
+		}
+	}
 }
