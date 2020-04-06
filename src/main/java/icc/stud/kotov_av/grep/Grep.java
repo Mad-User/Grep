4624bbc029a8c73f4ee0edf6e92c7efbbcc6b271
Mad-User <kotov.andrey01@mail.ru>
2020-04-06 19:21:09 +0300

    remove: Grep. rename: FileHandler->Grep refactor

--- src/main/java/icc/stud/kotov_av/grep/Grep.java
+++ src/main/java/icc/stud/kotov_av/grep/Grep.java
@@ -2,17 +2,64 @@ package icc.stud.kotov_av.grep;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Grep {
-  public static void main(final String[] args) {
-    ArgumentsHolder ah = new ArgumentsHolder(args);
-    FileHandler fh = new FileHandler();
+    private ArrayList<String> lines;
+    private ArgumentsHolder ah;
+    private BufferedReader br;
 
-    try (BufferedReader br = new BufferedReader(new FileReader(ah.inputFile))) {
-      fh.execute(ah, br);
-      fh.print();
-    } catch (Exception ex) {
-      System.out.println(ex.getMessage());
+    public Grep(String[] args) throws Exception {
+        lines = new ArrayList<String>();
+        ah = new ArgumentsHolder(args);
+        br = new BufferedReader(new FileReader(ah.inputFile));
+        execute();
     }
-  }
+
+    public void print() {
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+
+    public String[] getLines() {
+        String[] output = new String[lines.size()];
+        output = lines.toArray(output);
+        return output;
+    }
+
+    public void execute() throws Exception {
+        int flags = ah.caseIgnore ? Pattern.CASE_INSENSITIVE : 0;
+        Pattern pattern = ah.regex ? Pattern.compile(ah.word, flags) : null;
+
+        String search = ah.caseIgnore ? ah.word.toLowerCase() : ah.word;
+
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            if (pattern != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.matches() ^ ah.inversion)
+                    lines.add(line);
+            } else {
+                if (ah.caseIgnore) {
+                    if (line.toLowerCase().contains(search) ^ ah.inversion)
+                        lines.add(line);
+                } else {
+                    if (line.contains(search) ^ ah.inversion)
+                        lines.add(line);
+                }
+            }
+        }
+    }
+
+    public static void main(final String[] args) {
+        try {
+            Grep fh = new Grep(args);
+            fh.print();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
